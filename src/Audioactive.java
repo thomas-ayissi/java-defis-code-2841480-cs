1a8b7fc195b2b71175884f6f413237f625543449
Thomas Ayissi <thomas8sea@gmail.com>
2024-03-15 21:37:23 +0000

    essi 1 - exercice 2

--- src/Audioactive.java
+++ src/Audioactive.java
@@ -1,6 +1,26 @@
 public class Audioactive {
 
   public static long valeurSuivante(long valeur) {
-    throw new UnsupportedOperationException("Essayez votre propre solution ici !!!");
+    throw new UnsupportedOperationException(fonctionDeTri);
+          for (i=0, i=n, i++ ){
+            if (i=i+1){
+              return i;
+              Sytem.out.print(1i)
+               else if {
+                  while(i=i+1){
+                    i++
+                    return i;
+                    Sytem.out.print(i n)
+                    //ma vieille erreur : je m'emmèle les pinceaux entre i qui est l'indice sur le vecteur 
+                    //et la variable située à la position i[1] par exemple
+                    //le mieux serait sans doute de déclarer un vecteur ici avant de commencer
+                  }
+
+               }
+
+
+
+            }
+          }
   }
 }
